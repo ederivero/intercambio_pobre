3e51e17525e6ae48081e1b05caf6886b8acee759
Eduardo de Rivero <eduardomanrique@ravn.co>
2024-12-26 10:27:12 -0500

    fixed

--- src/datasource.ts
+++ src/datasource.ts
@@ -4,10 +4,10 @@ import { config } from 'dotenv'
 
 config()
 export const AppDataSource = new DataSource({
-  type: 'postgres', // Cambia según tu base de datos
+  type: 'postgres',
   url: process.env.DATABASE_URL,
-  synchronize: false, // Usa migraciones, no sincronización automática
+  synchronize: false,
   logging: true,
-  entities: ['src/models/index.ts'], // Ruta a tus entidades
-  migrations: ['src/migrations/*.ts'], // Ruta a tus migraciones
+  entities: ['src/models/index.ts'],
+  migrations: ['src/migrations/*.ts'],
 })
